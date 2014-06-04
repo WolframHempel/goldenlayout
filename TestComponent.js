adcf63d8715d1326bf0aef49892406d996182065
Wolfram Hempel <hempel.wolfram@gmail.com>
2014-06-04 21:08:52 +0100

    Latest

--- TestComponent.js
+++ TestComponent.js
@@ -4,13 +4,17 @@ TestComponent = function( container, config ) {
 	this._config = config;
 	TestComponent.id++;
 	this._element = $('<div class="testComponent"></div>' );
-	this._element.append( '<h1>' + config.componentName + ' ' + TestComponent.id + '</h1>' );
+	this._sizeIndicator = $('<div class="sizeIndicator"></div>' );
+	this._element.append( this._sizeIndicator );
 	//this._element.append( '<h3>' + JSON.stringify( config ) + '</h3>' );
 	this._element.append(
-		'<ul>' +
-			'<li><label>height</label><span class="height"></span></li>' +
-			'<li><label>width</label><span class="width"></span></li>' +
-		'</ul>'
+		'<div class="content">' +
+			'<h1>' + config.componentName + ' ' + TestComponent.id + '</h1>' +
+			'<ul>' +
+				'<li><label>height</label><span class="height"></span></li>' +
+				'<li><label>width</label><span class="width"></span></li>' +
+			'</ul>' +
+		'</div>'
 	);
 
 	container.getElement().append( this._element );
@@ -22,6 +26,7 @@ TestComponent.id = 0;
 
 TestComponent.prototype.resize = function() {
 	this._element.width( this._container.width ).height( this._container.height );
+	this._sizeIndicator.width( this._container.width - 12 ).height( this._container.height - 12 );
 	this._element.find( '.width' ).text( this._container.width );
 	this._element.find( '.height' ).text( this._container.height );
 };
